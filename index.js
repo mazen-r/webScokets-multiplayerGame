e26fafb4d82f16930ee33302c80b305fab1ef675
Mazen Ramadan <mazenramadan00@gmail.com>
2022-04-13 16:22:25 +0200

    connecting client

--- index.js
+++ index.js
@@ -9,4 +9,17 @@ const wsServer = new websocketServer({
 wsServer.on("request", request => {
     //connecting
     const connection = request.accept(null, request.origin);
+    connection.on("open", () => console.log("opened"))
+    connection.on("close", () => console.log("closed"))
+    connection.on("message", message => {
+
+    })
 })
+
+
+
+function S4() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+}
+// then to call it, plus stitch in '4' in the third group
+const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
